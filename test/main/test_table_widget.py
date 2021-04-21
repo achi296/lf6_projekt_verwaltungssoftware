97e803d502096a58d9c42bdaec30e756a72ceed5
dominik-schumbert <64913353+dominik-schumbert@users.noreply.github.com>
2021-04-21 16:00:12 +0200

    Adding a test table

---
+++ test/main/test_table_widget.py
@@ -0,0 +1,56 @@
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import Qt
+import pandas as pd
+
+
+class TableModel(QtCore.QAbstractTableModel):
+
+    def __init__(self, data):
+        super(TableModel, self).__init__()
+        self._data = data
+
+    def data(self, index, role):
+        if role == Qt.DisplayRole:
+            value = self._data.iloc[index.row(), index.column()]
+            return str(value)
+
+    def rowCount(self, index):
+        return self._data.shape[0]
+
+    def columnCount(self, index):
+        return self._data.shape[1]
+
+    def headerData(self, section, orientation, role):
+        # section is the index of the column/row.
+        if role == Qt.DisplayRole:
+            if orientation == Qt.Horizontal:
+                return str(self._data.columns[section])
+
+            if orientation == Qt.Vertical:
+                return str(self._data.index[section])
+
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setFixedSize(350, 150)
+        self.table = QtWidgets.QTableView()
+
+        data = pd.DataFrame({
+          "A": [1, 9, 2],
+          "B": [1, 0, -1],
+          "C": [3, 5, 2]
+        }, index=['Row 1', 'Row 2', 'Row 3'])
+
+        self.model = TableModel(data)
+        self.table.setModel(self.model)
+
+        self.setCentralWidget(self.table)
+
+
+app=QtWidgets.QApplication(sys.argv)
+window=MainWindow()
+window.show()
+app.exec_()
