e6134ebd10221770ec6d2f07de45049b429b4b99
dominik-schumbert <64913353+dominik-schumbert@users.noreply.github.com>
2021-04-21 17:08:59 +0200

    Adding a setup MySQL script in order to initialise the database

--- setup_mysql.sh
+++ setup_mysql.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
+# The parameter for this file is the password of the root user
+# $1 = root password for MySQL instance
+
 # create the new database "verwaltung"
 echo -e "Creating the new database verwaltung\n"
-mysql -h localhost -u root < database/create_database.sql
+mysql -h localhost -u root -p $1 < database/create_database.sql
 # create a new user "pyuser" and set the permissions
 echo -e "Creating a new user pyuser\n"
-mysql -h localhost -u root < database/setup_permissions.sql
+mysql -h localhost -u root -p $1 < database/setup_permissions.sql
 # create the tables
 declare -i I=1
 MAX=$(ls "database/ddl/" | wc -l)
 for SCRIPT in database/ddl/*.sql
 do
   echo -e "Executing script (${I}/${MAX}): ${SCRIPT}\n"
-  mysql -h localhost -u root < $SCRIPT
+  mysql -h localhost -u root -p $1 < $SCRIPT
   I=$I+1
 done
