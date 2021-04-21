0cfd393863ab2ae5aaae98e528197173cbf3b416
dominik-schumbert <64913353+dominik-schumbert@users.noreply.github.com>
2021-04-21 11:03:42 +0200

    Adding test insert into for optical discs

---
+++ test/dml/insert_into_optical_discs.sql
@@ -0,0 +1,23 @@
+USE verwaltung;
+INSERT INTO optical discs (
+    type,
+    title,
+    published_date,
+    genre,
+    price,
+    description,
+    fsk,
+    ean
+)
+VALUES
+(
+'DVD',
+'Star Wars Episode VII',
+'2015-09-01',
+'Science-Fiction',
+19.99,
+'Star Wars: Die letzten Jedi/Zusammenfassung
+'Luke Skywalker hat sich im Alter auf eine einsame Insel zurückgezogen.Doch mit dem Frieden ist es vorbei, als Rey auftaucht, um von ihm den Umgang mit der Macht zu erlernen. Ihr Wunsch ist es, von ihm in der Kunst der Jedi ausgebildet zu werden. Luke ist zu einer Entscheidung gezwungen, die Reys Leben für immer verändern wird. Unterdessen liefern sich Kylo Ren und seine Erste Ordnung einen erbitterten Kampf mit Leia und den Kämpfern des Widerstands um die Vorherrschaft in der Galaxie.',
+12,
+'8717418565114'
+)
